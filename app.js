e045015494710fda52fc7776c5577b777212cc54
ahsansamad <ingeniouslogo@gmail.com>
2024-05-22 16:50:37 +0500

    setinterval

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var hours = document.getElementById("hours");
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
 // ================= Calculations-Started ===============/
-var date = new Date().getTime();
-var eid = new Date("17 june 2024").getTime();
-var conversion = eid - date;
-months.value = Math.floor(conversion / (1000 * 60 * 60 * 24 * 31));
-days.value = Math.floor(conversion / (1000 * 60 * 60 * 24) - 31).toString();
-if (days.value == -1) {
-  days.value = "0";
+function calculation() {
+  var date = new Date().getTime();
+  var eid = new Date("17 june 2024").getTime();
+  var conversion = eid - date;
+  months.value = Math.floor(conversion / (1000 * 60 * 60 * 24 * 31));
+  days.value = Math.floor((conversion / (1000 * 60 * 60 * 24)) % 31).toString();
+  if (days.value == -1) {
+    days.value = "0";
+  }
+  hours.value = Math.floor((conversion / (1000 * 60 * 60)) % 24);
+  minutes.value = Math.floor((conversion / (1000 * 60)) % 60);
+  seconds.value = Math.floor(conversion / 1000) % 60;
 }
-hours.value = Math.floor((conversion / (1000 * 60 * 60)) % 24);
-minutes.value = Math.floor((conversion / (1000 * 60)) % 60);
-seconds.value = Math.floor(conversion / 1000) % 60;
 // ================= Calculations-End ===============/
+setInterval(calculation, 1000);
 // console.log();
